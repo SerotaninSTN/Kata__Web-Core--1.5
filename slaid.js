ca72ffc877c2fe7d9a1e75468d238fa2f8fcc72c
Татьяна <119400671+SerotaninSTN@users.noreply.github.com>
2023-02-01 14:16:56 +0300

    Update slaid.js

--- slaid.js
+++ slaid.js
@@ -1,12 +1,11 @@
 const btn = document.querySelector('.swiper-button')
 const btnHide = document.querySelector('.swiper-button--hide')
-const slides = Array.from(document.querySelectorAll('.swiper-slide'))
 const swiperBrend = document.querySelector('.page-main__swiper-brend')
 const swiperContainer = document.querySelector('.swiper-container')
 
 function openSlides() {
     btn.addEventListener('click', () => {
-        slides.forEach(item => item.classList.remove('hidden'));
+     
         btn.classList.add('hidden');
         btnHide.classList.remove('hidden');
         swiperBrend.classList.add('page-main__swiper-brend--open')
@@ -16,11 +15,6 @@ function openSlides() {
 
 function hideSlides(count) {
     btnHide.addEventListener('click', () => {
-        slides.forEach((item, index) => {
-            item.classList.add('hidden');
-
-            if (index <= count) item.classList.remove('hidden');
-        })
         btnHide.classList.add('hidden');
         btn.classList.remove('hidden');
         swiperBrend.classList.remove('page-main__swiper-brend--open')
@@ -31,40 +25,11 @@ function hideSlides(count) {
 
 
 
-function responsel1() {
-if (window.innerWidth>1119) {
-    
-    const ind = 7;
-    slides.forEach((item, index) => {
-        item.classList.add('hidden')
-        if (index <= ind) {
-            item.classList.remove('hidden')
-        } else if (index > ind) {
-            btn.classList.remove('hidden');
-        }
+function responsel() {
+
         openSlides();
-        hideSlides(ind);
-})
-}
+        hideSlides();
 }
 
-responsel1()
+responsel()
 
-
-function responsel2() {
-    if (window.innerWidth <=1119 && window.innerWidth > 767) {
-        const ind = 5;
-        slides.forEach((item, index) => {
-            item.classList.add('hidden')
-            if (index <= ind) {
-                item.classList.remove('hidden')
-            } else if (index > ind) {
-                btn.classList.remove('hidden');
-            }
-            openSlides();
-            hideSlides(ind);
-           
-           })   
-    }
-}
-responsel2()
