cb71500154cf49e4eb11b6e4a1ed3b98e936603b
Татьяна <119400671+SerotaninSTN@users.noreply.github.com>
2023-02-02 23:39:12 +0300

    Update slaid.js

--- slaid.js
+++ slaid.js
@@ -1,35 +1,22 @@
 const btn = document.querySelector('.swiper-button')
-const btnHide = document.querySelector('.swiper-button--hide')
 const swiperBrend = document.querySelector('.page-main__swiper-brend')
 const swiperContainer = document.querySelector('.swiper-container')
 
-function openSlides() {
-    btn.addEventListener('click', () => {
-     
-        btn.classList.add('hidden');
-        btnHide.classList.remove('hidden');
-        swiperBrend.classList.add('page-main__swiper-brend--open')
-        swiperContainer.classList.add('swiper-container--open')
-})
-}
-
-function hideSlides(count) {
-    btnHide.addEventListener('click', () => {
-        btnHide.classList.add('hidden');
-        btn.classList.remove('hidden');
-        swiperBrend.classList.remove('page-main__swiper-brend--open')
-        swiperContainer.classList.remove('swiper-container--open')
-    })
-}
-
-
-
-
-function responsel() {
-
-        openSlides();
-        hideSlides();
-}
-
-responsel()
-
+    btn.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        if (btn.classList.contains('swiper-button')) {
+            btn.classList.remove('swiper-button');
+            btn.classList.add('swiper-button--hide');
+           
+            btn.textContent = 'Скрыть';
+            swiperBrend.classList.add('page-main__swiper-brend--open')
+            swiperContainer.classList.add('swiper-container--open')
+        } else if(btn.classList.contains('swiper-button--hide')) {
+            btn.classList.remove('swiper-button--hide');
+                btn.classList.add('swiper-button');
+                btn.textContent = 'Показать все';
+                swiperBrend.classList.remove('page-main__swiper-brend--open')
+                swiperContainer.classList.remove('swiper-container--open')
+        }
+      });
+      
